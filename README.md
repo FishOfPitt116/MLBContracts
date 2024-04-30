89a85ac7b5743a3820dcd37b3d9420c625be27b0
Robbie Fishel <robbie.fishel6@gmail.com>
2024-04-30 17:22:09 -0400

    added dataset, generation script, and information

---
+++ README.md
@@ -0,0 +1,20 @@
+# MLB Contracts Repository
+
+## Dataset
+
+### Contracts Dataset
+
+The contracts dataset was assembled using contract information from [Cot's Baseball Contracts](https://legacy.baseballprospectus.com/compensation/cots/). The dataset currently includes player contract information from 2020 through 2024.
+
+To regenerate the dataset, place all spreadsheets from Cot's Contracts in a directory called `contract_data`, and then run the script `python parse.py`. Make sure that the name of each file is structured `MLB {team} {year}.csv`. The dataset will be generated in the `dataset` directory.
+
+Columns:
+- first name
+- last name
+- team
+- year
+- position
+- age
+- service time
+- agent
+- value: salary in millions of dollars
---
+++ parse.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+
+DIRECTORY = "contract_data"
+FINAL_DATASET = "dataset/contracts.csv"
+
+contracts_df = pd.DataFrame(columns=["first_name", "last_name", "team", "year", "position", "age", "service time", "agent", "value"])
+
+for filename in os.listdir(DIRECTORY):
+    filename_split = filename.split(".")[0].split()
+    team = filename_split[1]
+    year = f"20{filename_split[2]}"
+    filepath = os.path.join(DIRECTORY, filename)
+    df = pd.read_csv(filepath)
+    if int(year) >= 2020:
+        i = 8
+        while pd.notna(df.iloc[i, 12]):
+            if df.iloc[i, 12] != "forfeited":
+                contracts_df.loc[len(contracts_df)] = {
+                    "first_name" : df.iloc[i, 0].split(", ")[1],
+                    "last_name" : df.iloc[i, 0].split(", ")[0],
+                    "team" : team,
+                    "year" : year,
+                    "position" : df.iloc[i, 1],
+                    "age" : df.iloc[i, 5],
+                    "service time" : df.iloc[i, 6],
+                    "agent" : df.iloc[i, 8],
+                    "value" : int(df.iloc[i, 12][1:].replace(",", "")) / pow(10, 6)
+                }
+            i += 1
+    else:
+        print("old spreadsheet format")
+contracts_df.to_csv(FINAL_DATASET)
