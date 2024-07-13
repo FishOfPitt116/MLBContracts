13e9d539f0e9f4845a55337512454ca5354f13bd
Robbie Fishel <robbie.fishel6@gmail.com>
2024-07-13 16:46:38 -0400

    established isolated method for getting merged dfs

--- src/main.py
+++ src/main.py
@@ -29,22 +29,28 @@ BATTING_STATS = pd.read_csv(os.path.join(DATASET_DIR, "batting_stats.csv"))
 CONTRACTS = pd.read_csv(os.path.join(DATASET_DIR, "contracts.csv"))
 PITCHING_STATS = pd.read_csv(os.path.join(DATASET_DIR, "pitching_stats.csv"))
 
-BATTING_CONTRACTS = pd.DataFrame(columns=CONTRACTS.columns)
-PITCHING_CONTRACTS = pd.DataFrame(columns=CONTRACTS.columns)
-
 def filter_by_position(pos):
     return CONTRACTS.query(f'position == \"{pos}\"')
 
-if __name__ == "__main__":
+def get_merged_dfs():
+    batting_contracts = pd.DataFrame(columns=CONTRACTS.columns)
+    pitching_contracts = pd.DataFrame(columns=CONTRACTS.columns)
+
     for position in CONTRACTS["position"].unique():
         pos_df = filter_by_position(position)
         if position in ["rhp-s", "lhp-s", "rhp-c", "lhp-c", "rhp", "lhp"]:
-            PITCHING_CONTRACTS = PITCHING_CONTRACTS._append(pos_df, ignore_index=False)
+            pitching_contracts = pitching_contracts._append(pos_df, ignore_index=False)
         elif "lhp" in position or "rhp" in position:
-            pass
+            pass # shohei case
         else:
-            BATTING_CONTRACTS = BATTING_CONTRACTS._append(pos_df, ignore_index=False)
-    batting_data = pd.merge(BATTING_CONTRACTS, BATTING_STATS, left_on="Unnamed: 0", right_on="Index")
-    pitching_data = pd.merge(PITCHING_CONTRACTS, PITCHING_STATS, left_on="Unnamed: 0", right_on="Index")
-    print(batting_data.columns)
-    print(pitching_data.columns)
+            batting_contracts = batting_contracts._append(pos_df, ignore_index=False)
+
+    batting_data = pd.merge(batting_contracts, BATTING_STATS, left_on="Unnamed: 0", right_on="Index")
+    pitching_data = pd.merge(pitching_contracts, PITCHING_STATS, left_on="Unnamed: 0", right_on="Index")
+
+    return batting_data, pitching_data
+
+if __name__ == "__main__":
+    batting_data, pitching_data = get_merged_dfs()
+    print(batting_data)
+    print(pitching_data)
