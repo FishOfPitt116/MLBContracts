abe8587126e67ac065ab2775a5f63c33aaac79a7
Robbie Fishel <robbie.fishel6@gmail.com>
2025-03-20 19:37:44 -0400

    establish method for player ID

--- data_generation/spotrac.py
+++ data_generation/spotrac.py
@@ -24,6 +24,13 @@ def fetch_spotrac_data(url: str) -> BeautifulSoup:
 def sanitize_string(s: str) -> str:
     return s.replace("\n", "").strip()
 
+def get_player_id(columns: List[Tag], headers: Dict[str, int]) -> str:
+    name = sanitize_string(columns[headers['player']].get_text()).split(" ", 1)
+    spotrac_link = sanitize_string(columns[headers['player']].find("a")["href"])
+    link_id = spotrac_link.split("/")[-1]
+
+    return f"{name[1]}_{link_id}"
+
 def get_table_headers(table: Tag) -> List[str]:
     print( [ sanitize_string(header.get_text()).replace("$", " ").split(" ")[0].lower() for header in table.find("thead").find_all("th") ] )
     return { sanitize_string(header.get_text()).replace("$", " ").split(" ")[0].lower() : i for i, header in enumerate(table.find("thead").find_all("th")) }
@@ -31,11 +38,11 @@ def get_table_headers(table: Tag) -> List[str]:
 def extract_player_data(row: Tag, headers: Dict[str, int]) -> Player:
     columns = row.find_all('td')
     name = sanitize_string(columns[headers['player']].get_text()).split(" ", 1)
+    player_id = get_player_id(columns, headers)
     spotrac_link = sanitize_string(columns[headers['player']].find("a")["href"])
-    link_id = spotrac_link.split("/")[-1]
-    player_id = f"{name[1]}_{link_id}"
     if player_id in PLAYER_OBJECT_CACHE:
         return PLAYER_OBJECT_CACHE[player_id]
+    # TODO: check spotrac link for birthday
     return Player(
         player_id=player_id,
         first_name=name[0],
