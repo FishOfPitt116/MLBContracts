aa1e0b3e432a16ec8624dcf3ca6ed385d930c645
Robbie Fishel <robbie.fishel6@gmail.com>
2025-04-03 23:31:30 -0400

    update players dataset with birthday info

--- data_generation/cleanup.py
+++ data_generation/cleanup.py
@@ -1,6 +1,10 @@
-import re
-from save import read_players_from_file, write_players_to_file, read_contracts_from_file, write_contracts_to_file
+from bs4 import BeautifulSoup
+from datetime import datetime
 from pybaseball import playerid_reverse_lookup
+import re
+import requests
+from save import read_players_from_file, write_players_to_file, read_contracts_from_file, write_contracts_to_file
+import time
 
 def clean_players_with_qo():
     players = read_players_from_file()
@@ -42,6 +46,36 @@ def update_baseball_reference_links():
     write_players_to_file(players, overwrite=True)
     print(f"Updated {len(lookup_map)} players with missing baseball reference links.")
 
+def update_player_birthdays():
+    players = read_players_from_file()
+    players_to_update = [player for player in players if not player.birth_date and player.baseball_reference_link]
+    
+    if not players_to_update:
+        print("No players missing birth dates.")
+        return
+    
+    updated_players = 0
+    for player in players_to_update:
+        print(f"Updating birthday for {player.first_name} {player.last_name}")
+        response = requests.get(player.baseball_reference_link)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            birth_element = soup.find('span', id='necro-birth')
+            print(f"Birth element found: {birth_element}")
+            if birth_element and 'data-birth' in birth_element.attrs:
+                player.birth_date = datetime.strptime(birth_element['data-birth'], "%Y-%m-%d")
+                updated_players += 1
+            print(f"Updated birthday for {player.first_name} {player.last_name}: {player.birth_date}")
+        elif response.status_code == 429:
+            print(f"Rate limit exceeded for {player.first_name} {player.last_name}. Exiting loop.")
+            print(response.text)
+            break
+        time.sleep(4) # to avoid rate limiting
+    
+    write_players_to_file(players, overwrite=True)
+    print(f"Updated {updated_players} players with missing birth dates.")
+
 if __name__ == "__main__":
     clean_players_with_qo()
     update_baseball_reference_links()
+    update_player_birthdays()
